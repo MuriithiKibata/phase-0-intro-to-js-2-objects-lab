c94e87ccc51a6f1cf84351761f9eb1bbae743329
Muriithi <muriithi.kibata@student.moringaschool.com>
2022-08-08 10:43:37 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+let employee = {
+    name: 'Sam',
+    streetAddress: '11 Broadway',
+
+}
+let copyOf = {...employee}
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return copyOf
+
+}
+//employee.streetAddress = '12 Broadway'
+function  destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    //employee['streetAddress'] = "12 Broadway"
+    employee.streetAddress = '12 Broadway'
+    return employee
+
+}
+let newEmployee = {...employee}
+ //newEmployee.delete = 'name'
+function  deleteFromEmployeeByKey(employee, key) {
+  delete newEmployee ["name"]
+    return newEmployee
+
+}
+function  destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee["name"]
+    return employee
+}
